e04f950835480c292c3053720736cc18a6c2c00e
rajnish <rajneeshkumar9674737@gmail.com>
2024-09-06 14:28:03 +0530

    rm:key

--- src/app/api/score/[score]/route.ts
+++ src/app/api/score/[score]/route.ts
@@ -3,18 +3,18 @@ import jwt from 'jsonwebtoken';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { cookies } from "next/headers";
 
-const supabaseUrl = 'https://eieoyqffboqdrvgcsmyf.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY || "";
-const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
+// const supabaseUrl = 'https://eieoyqffboqdrvgcsmyf.supabase.co';
+// const supabaseKey = process.env.SUPABASE_KEY || "";
+// const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 
 export async function GET(request: NextRequest, { params }: { params: { score: number } }) {
   try {
     // Verify database connection
-    const { error: connectionError } = await supabase.from('scores').select('*').limit(1);
-    if (connectionError) {
-      console.error("Error connecting to Supabase:", connectionError.message);
-      return NextResponse.json({ success: false, message: "Database connection failed" }, { status: 500 });
-    }
+    // const { error: connectionError } = await supabase.from('scores').select('*').limit(1);
+    // if (connectionError) {
+    //   console.error("Error connecting to Supabase:", connectionError.message);
+    //   return NextResponse.json({ success: false, message: "Database connection failed" }, { status: 500 });
+    // }
 
     // Retrieve the score from params
     const { score } = params;
@@ -44,14 +44,14 @@ export async function GET(request: NextRequest, { params }: { params: { score: n
     });
 
     // Insert score into Supabase
-    const { data, error } = await supabase
-      .from('scores') // Replace 'scores' with your actual table name
-      .insert([{ score, created_at: new Date().toISOString() }]);
+    // const { data, error } = await supabase
+    //   .from('scores') // Replace 'scores' with your actual table name
+    //   .insert([{ score, created_at: new Date().toISOString() }]);
 
-    if (error) {
-      console.error("Error inserting data:", error.message);
-      throw error;
-    }
+    // if (error) {
+    //   console.error("Error inserting data:", error.message);
+    //   throw error;
+    // }
 
     return response;
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,9 +15,27 @@ export default function Home() {
       // Update max score if necessary
       setMaxScore((prevMaxScore) => Math.max(prevMaxScore, score));
       localStorage.setItem("maxScore", maxScore.toString());
+        // todo 
+    // fetch(/api/score/score, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    // }).then((res) => res.json()).then((data) => {
+    //   setScore(data.data.score);
+    // });
 
       // Reset score after game over
       setScore(0);
+        // todo 
+    // fetch(/api/getScore, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    // }).then((res) => res.json()).then((data) => {
+    //   setScore(data.data.score);
+    // });
 
       // Calculate and set game duration
       const time = new Date();
@@ -45,7 +63,7 @@ export default function Home() {
     <>
       {isGameOver ? (
         <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex justify-center items-center text-center">
-          <div className="w-1/3 max-w-64 bg-[#FFF7FC] bg-opacity-60 backdrop-blur-sm text-[#01204E] px-6 py-3 rounded-lg">
+          <div className="w-1/3 min-w-64 max-w-96 bg-[#FFF7FC] bg-opacity-60 backdrop-blur-sm text-[#01204E] px-6 py-3 rounded-lg">
             <h1 className="text-2xl font-bold">Welcome to the game</h1>
             <p>Score: {maxScore}</p>
             <button onClick={handleStart} className="px-4 py-2 border rounded-md">Start Game</button>
